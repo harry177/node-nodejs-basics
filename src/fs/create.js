827bd0a2cdaf2df4ee3f3e2f51abb31415aa4627
Harry177 <harry177@rambler.ru>
2023-06-16 01:37:12 +0200

    feat: solve first fs subtask

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import fs from "fs";
+const content = "I am fresh and young";
+
 const create = async () => {
-    // Write your code here 
+  const file = "./src/fs/files/fresh.txt";
+  if (fs.existsSync(file)) {
+    console.log("FS operation failed");
+  } else {
+    fs.writeFile(file, content, (err) => {
+      if (err) {
+        console.log("Something went wrong...");
+      }
+    });
+  }
 };
 
 await create();
