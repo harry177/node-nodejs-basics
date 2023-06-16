7573a65be3a923193eccaa5d4c6cf3b291de7335
Harry177 <harry177@rambler.ru>
2023-06-17 01:21:57 +0200

    feat: complete cli subtasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,9 @@
 const parseArgs = () => {
-    // Write your code here 
+    for (let key of process.argv) {
+        if (key.includes('--')) {
+            console.log(`${key.slice(2)} is ${process.argv[process.argv.indexOf(key) + 1]}`)
+        }
+    }
 };
 
 parseArgs();
