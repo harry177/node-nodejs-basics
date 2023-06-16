dfc8266ca587317878c8adacfc9772583ad14f3f
Harry177 <harry177@rambler.ru>
2023-06-16 21:22:14 +0200

    feat: solve copy subtask of fs section

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,18 @@
+import fs from "fs";
+
+const folderName = "./src/fs/files";
+const newFolderName = "./src/fs/files_copy";
+
 const copy = async () => {
-    // Write your code here 
+  if (!fs.existsSync(newFolderName)) {
+    fs.cp(folderName, newFolderName, { recursive: true }, (err) => {
+      if (err) {
+        console.error("FS operation failed");
+      }
+    });
+  } else {
+    console.error("FS operation failed");
+  }
 };
 
 await copy();
