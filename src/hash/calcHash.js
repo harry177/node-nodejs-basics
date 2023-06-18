d9f9b495e5d6f69754e42d30ff3c5d3a527a90a4
Harry177 <harry177@rambler.ru>
2023-06-18 23:51:57 +0200

    feat: complete hash subtask

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,11 @@
+import fs from "fs";
+import { createHash } from "crypto";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash("sha256");
+  const target = fs.readFileSync("./src/hash/files/fileToCalculateHashFor.txt");
+  hash.update(target);
+  console.log(hash.digest("hex"));
 };
 
 await calculateHash();
