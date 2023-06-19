9915ae00702dd49db617a1844b30724ac9ff9f5c
Harry177 <harry177@rambler.ru>
2023-06-19 23:23:16 +0200

    feat: complete zlib subtasks

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,26 @@
+import fs from "fs";
+import { createGunzip } from "zlib";
+import { pipeline } from "stream";
+
+const targetFile = "./src/zip/files/archive.gz";
+const distFile = "./src/zip/files/fileToCompress.txt";
+
 const decompress = async () => {
-    // Write your code here 
+  const gunzip = createGunzip();
+  const readableStream = fs.createReadStream(targetFile);
+  const writableStream = fs.createWriteStream(distFile, "utf8");
+
+  pipeline(readableStream, gunzip, writableStream, (err) => {
+    if (err) {
+      console.error("An error occurred:", err);
+      process.exitCode = 1;
+    }
+  });
+  fs.unlink(targetFile, (err) => {
+    if (err) {
+      console.error("FS operation failed");
+    }
+  });
 };
 
 await decompress();
