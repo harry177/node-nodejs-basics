7fecc80fd997d5ed993f97887f0d8c0748df6e80
Harry177 <harry177@rambler.ru>
2023-06-20 01:44:59 +0200

    feat: try to implement worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,19 @@
-const performCalculations = async () => {
-    // Write your code here
+import os from "os";
+import { Worker } from "worker_threads";
+
+const result = [];
+
+const performCalculations = async (workerData) => {
+  const numOfCpus = os.cpus().length;
+  
+  for (let i = 0; i < numOfCpus; i++) {
+    const worker = new Worker("./src/wt/worker.js", { workerData });
+    const n = i + 10;
+    worker.postMessage(n);
+    worker.on("message", (final) => result.push(final));
+    //console.log(result);
+  }
+  console.log(result);
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,15 @@
+import { parentPort, workerData, isMainThread } from "worker_threads";
+
 // n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+parentPort.on("message", (n) => {
+  const final = n < 2 ? n : n - 1 + n - 2;
+  const sendResult = () => {
+    parentPort.postMessage(final);
+  };
 
-const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
-};
+  sendResult();
+});
 
-sendResult();
+parentPort.postMessage({ start: workerData, isMainThread });
+
+//let result = nthFibonacci();
